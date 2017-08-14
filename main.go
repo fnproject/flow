95a135b381c3bb1a6b4a20f1e368811d5d69bbf1
Mark Godfrey <mark.godfrey@oracle.com>
2017-08-14 11:29:48 +0100

    Sketch out REST API, with no-op handlers.

--- main.go
+++ main.go
@@ -6,6 +6,25 @@ import (
 	"net/http"
 )
 
+func noOpHandler(c *gin.Context) {
+	c.Status(http.StatusNotFound)
+}
+
+func stageHandler(c *gin.Context) {
+	stageID := c.Param("stageId")
+	operation := c.Param("operation")
+	switch operation {
+	case "complete":
+		log.Info("Completing stage " + stageID)
+		noOpHandler(c)
+	case "fail":
+		log.Info("Failing stage " + stageID)
+		noOpHandler(c)
+	default:
+		log.Info("Stage operation " + operation)
+	}
+}
+
 func main() {
 
 	engine := gin.Default()
@@ -20,6 +39,20 @@ func main() {
 			log.Info("Requested graph with Id " + c.Param("graphId"))
 			c.Status(http.StatusNotFound)
 		})
+		graph.POST("/:graphId/supply", noOpHandler)
+		graph.POST("/:graphId/invokeFunction", noOpHandler)
+		graph.POST("/:graphId/completedValue", noOpHandler)
+		graph.POST("/:graphId/delay", noOpHandler)
+		graph.POST("/:graphId/allOf", noOpHandler)
+		graph.POST("/:graphId/anyOf", noOpHandler)
+		graph.POST("/:graphId/externalCompletion", noOpHandler)
+		graph.POST("/:graphId/commit", noOpHandler)
+
+		stage := graph.Group("/:graphId/stage")
+		{
+			stage.GET("/:stageId", noOpHandler)
+			stage.POST("/:stageId/:operation", stageHandler)
+		}
 	}
 
 	log.Info("Starting")

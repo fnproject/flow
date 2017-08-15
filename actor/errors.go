c8204221c922bf5bd23c243211b08061b7786fc2
gerardo_viedma <gerardo.viedma.nunez@oracle.com>
2017-08-15 16:04:28 +0100

    added actor errors

--- actor/errors.go
+++ actor/errors.go
@@ -2,6 +2,10 @@ package actor
 
 import "github.com/fnproject/completer/model"
 
-func NewInvalidGraphOperation(graphId string) *model.InvalidGraphOperation {
+func NewGraphNotFoundError(graphId string) *model.InvalidGraphOperation {
 	return &model.InvalidGraphOperation{GraphId: graphId, Error: "Graph not found"}
+}
+
+func NewGraphEventPersistenceError(graphId string) *model.InvalidGraphOperation {
+	return &model.InvalidGraphOperation{GraphId: graphId, Error: "Failed to persist event"}
 }

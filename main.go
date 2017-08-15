7466a86482da173af56621c92f51c39a1640bd57
Mark Godfrey <mark.godfrey@oracle.com>
2017-08-15 16:35:25 +0100

    Get stage result.

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 	"net/http"
+	"strconv"
 )
 
 var log = logrus.WithField("logger", "api")
@@ -94,6 +95,130 @@ func getGraphState(c *gin.Context) {
 	c.JSON(http.StatusOK, getFakeGraphStateResponse(request))
 }
 
+func getFakeStageResultResponse(request model.GetStageResultRequest) model.GetStageResultResponse {
+	return model.GetStageResultResponse{
+		GraphId: request.GraphId,
+		StageId: request.StageId,
+		Result: &model.CompletionResult{
+			Successful: true,
+			Datum: &model.Datum{
+				Val: &model.Datum_Empty{
+					Empty: &model.EmptyDatum{},
+				},
+			},
+		},
+	}
+}
+
+func getGraphStage(c *gin.Context) {
+	graphID := c.Param("graphId")
+	stageID := c.Param("stageId")
+
+	stageNumber, err := strconv.ParseUint(stageID, 10, 32)
+	if err != nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	request := model.GetStageResultRequest{
+		GraphId: graphID,
+		StageId: uint32(stageNumber),
+	}
+
+	// TODO: send to the GraphManager
+
+	response := getFakeStageResultResponse(request)
+
+	result := response.GetResult()
+	if result == nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	datum := result.GetDatum()
+	if datum == nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	myval := datum.GetVal()
+	if myval == nil {
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+
+	switch v := myval.(type) {
+	case *model.Datum_Error:
+		c.Header("FnProject-DatumType", "error")
+		resultStatus := "false"
+		if result.GetSuccessful() {
+			resultStatus = "true"
+		}
+		c.Header("FnProject-ResultStatus", resultStatus)
+		c.Header("FnProject-ErrorType", model.ErrorDatumType_name[int32(v.Error.GetType())])
+		c.String(http.StatusOK, v.Error.GetMessage())
+		return
+	case *model.Datum_Empty:
+		c.Header("FnProject-DatumType", "empty")
+		resultStatus := "false"
+		if result.GetSuccessful() {
+			resultStatus = "true"
+		}
+		c.Header("FnProject-ResultStatus", resultStatus)
+		c.Status(http.StatusOK)
+		return
+	case *model.Datum_Blob:
+		c.Header("FnProject-DatumType", "blob")
+		resultStatus := "false"
+		if result.GetSuccessful() {
+			resultStatus = "true"
+		}
+		c.Header("FnProject-ResultStatus", resultStatus)
+		c.Data(http.StatusOK, v.Blob.GetContentType(), v.Blob.GetDataString())
+		return
+	case *model.Datum_StageRef:
+		c.Header("FnProject-DatumType", "stageref")
+		resultStatus := "false"
+		if result.GetSuccessful() {
+			resultStatus = "true"
+		}
+		c.Header("FnProject-ResultStatus", resultStatus)
+		c.Header("FnProject-StageID", strconv.FormatUint(uint64(v.StageRef.StageRef), 32))
+		return
+	case *model.Datum_HttpReq:
+		c.Header("FnProject-DatumType", "httpreq")
+		resultStatus := "false"
+		if result.GetSuccessful() {
+			resultStatus = "true"
+		}
+		c.Header("FnProject-ResultStatus", resultStatus)
+
+		for _, header := range v.HttpReq.Headers {
+			c.Header("FnProject-Header-"+header.GetKey(), header.GetValue())
+		}
+		c.Header("FnProject-Method", model.HttpMethod_name[int32(v.HttpReq.GetMethod())])
+		c.Data(http.StatusOK, v.HttpReq.Body.GetContentType(), v.HttpReq.Body.GetDataString())
+		return
+	case *model.Datum_HttpResp:
+		c.Header("FnProject-DatumType", "httpreq")
+		resultStatus := "false"
+		if result.GetSuccessful() {
+			resultStatus = "true"
+		}
+		c.Header("FnProject-ResultStatus", resultStatus)
+
+		for _, header := range v.HttpResp.Headers {
+			c.Header("FnProject-Header-"+header.GetKey(), header.GetValue())
+		}
+		c.Header("FnProject-ResultCode", strconv.FormatUint(uint64(v.HttpResp.GetStatusCode()), 32))
+		c.Data(http.StatusOK, v.HttpResp.Body.GetContentType(), v.HttpResp.Body.GetDataString())
+		return
+	default:
+		c.Status(http.StatusInternalServerError)
+		return
+	}
+}
+
 func main() {
 
 	engine := gin.Default()
@@ -118,7 +243,7 @@ func main() {
 
 		stage := graph.Group("/:graphId/stage")
 		{
-			stage.GET("/:stageId", noOpHandler)
+			stage.GET("/:stageId", getGraphStage)
 			stage.POST("/:stageId/:operation", stageHandler)
 		}
 	}

ea757bd069daa5a2bf3a0c544ee2f0cf63c5eda0
Dario Domizioli <dario.domizioli@gmail.com>
2017-08-22 11:32:03 +0100

    Make release process similar to fn

---
+++ release.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -ex
+
+user="funcy"
+service="completer"
+tag="latest"
+
+# ensure working dir is clean
+git status
+if [[ -z $(git status -s) ]]
+then
+  echo "tree is clean"
+else
+  echo "tree is dirty, please commit changes before running this"
+  exit 1
+fi
+
+# git pull
+
+version_file="version/version.go"
+if [ -z $(grep -m1 '(?<= = ")\d+\.\d+\.\d+' $version_file) ]; then
+  echo "did not find semantic version in $version_file"
+  exit 1
+fi
+perl -i -pe 's/(?<= = ")\d+\.\d+\.\K(\d+)/$1+1/e' $version_file
+version=$(grep -m1 -Po '(?<= = ")\d+\.\d+\.\d+' $version_file)
+echo "Version: $version"
+
+make docker-build IMAGE_REPO_USER=$user IMAGE_NAME=$service IMAGE_VERSION=$version
+
+# git add -u
+# git commit -m "$service: $version release [skip ci]"
+# git tag -f -a "$version" -m "version $version"
+# git push
+# git push origin $version
+
+# Finally tag and push docker images
+#docker tag $user/$service:$tag $user/$service:$version
+#docker push $user/$service:$version
+#docker push $user/$service:$tag

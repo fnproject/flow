5b99e89f0203a584ff865315d9956b8d1589e257
Dario Domizioli <dario.domizioli@oracle.com>
2017-11-22 14:48:27 +0000

    Add trivial requests served and engine errors metrics to gin engine

--- server/metrics.go
+++ server/metrics.go
@@ -5,7 +5,36 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/gin-gonic/gin"
 )
+
+var (
+	httpRequestsServed = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "flow_service_http_requests",
+		Help: "Total number of frontend service http requests served",
+	})
+	httpErrorsCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "flow_service_http_errors",
+		Help: "Total number of frontend service http engine errors",
+	})
+)
+
+func init() {
+	prometheus.MustRegister(httpRequestsServed)
+	prometheus.MustRegister(httpErrorsCounter)
+}
+
+// engineMetrics is a Gin middleware that records things like number of errors directly from the http engine.
+func engineMetrics() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Next()
+		httpRequestsServed.Inc()
+		if len(c.Errors.Errors()) > 0 {
+			httpErrorsCounter.Inc()
+		}
+	}
+}
 
 // unaryMetricsInterceptor returns a new unary server interceptor for our own OpenTracing-based metrics.
 func unaryMetricsInterceptor() grpc.UnaryServerInterceptor {

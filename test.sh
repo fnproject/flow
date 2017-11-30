16c6b9d345167c512f0c942a2fd51899cc1bbc11
jan grant <jang@ioctl.org>
2017-11-30 12:07:15 +0000

    Add the go tools to the test path
    ... although they seem to have run spuriously on the failing build.
    (Artifact of the git checkout I think.)

--- test.sh
+++ test.sh
@@ -7,6 +7,7 @@ fi
 
 mkdir -p $TEST_RESULTS
 go get -u -v github.com/jstemmer/go-junit-report
+export PATH=$GOPATH/bin:$PATH
 make test | tee ${TEST_RESULTS}/go-test.out
 $GOPATH/bin/go-junit-report <${TEST_RESULTS}/go-test.out > ${TEST_RESULTS}/go-test-report.xml
 

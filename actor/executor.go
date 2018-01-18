3fc900fc3238cc8fb2fb62482f277cd90c2a6cd0
gerardo_viedma <gerardo.viedma.nunez@oracle.com>
2018-01-18 10:55:03 +0000

    logging improvements in executor actor

--- actor/executor.go
+++ actor/executor.go
@@ -87,7 +87,7 @@ func (exec *graphExecutor) HandleInvokeStage(msg *model.InvokeStageRequest) *mod
 		Closure: msg.GetClosure(),
 	}
 	buf := new(bytes.Buffer)
-	writer := jsonpb.Marshaler{EmitDefaults:true, OrigName: true}
+	writer := jsonpb.Marshaler{EmitDefaults: true, OrigName: true}
 	err := writer.Marshal(buf, runtimeRequest)
 	if err != nil {
 		return stageFailed(msg, model.ErrorDatumType_stage_failed, "Could not marshal the runtime invoke stage request message.", "")
@@ -129,8 +129,8 @@ func (exec *graphExecutor) HandleInvokeStage(msg *model.InvokeStageRequest) *mod
 
 	result := runtimeResponse.GetResult()
 	if result == nil {
-		stageLog.WithField("fn_call_id", callID).WithField("fn_lb_delay", lbDelayHeader).Error("Failed to read result from functions service: ", err)
-		return stageFailed(msg, model.ErrorDatumType_invalid_stage_response, "Failed to read result from functions service", callID)
+		stageLog.WithField("fn_call_id", callID).WithField("fn_lb_delay", lbDelayHeader).Error("Empty result from functions service")
+		return stageFailed(msg, model.ErrorDatumType_invalid_stage_response, "Empty result from functions service", callID)
 	}
 
 	stageLog.WithField("fn_call_id", callID).WithField("fn_lb_delay", lbDelayHeader).WithField("successful", fmt.Sprintf("%t", result.Successful)).Info("Got stage response")
